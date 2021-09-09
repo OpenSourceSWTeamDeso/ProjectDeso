5615f5e7b6a717536fd37afb03df31e6523509bc
KiHo Kim <75569308+kiho0042@users.noreply.github.com>
2021-09-09 20:49:15 +0900

    Add files via upload

--- frontend/routes/homeStack.js
+++ frontend/routes/homeStack.js
@@ -2,15 +2,10 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import MainScreen from "../screens/MainScreen";
 import Result from "../screens/Result";
-import Select from "../screens/Select";
 
 const screens = {
   MainScreen: {
     screen: MainScreen,
-  },
-
-  Select: {
-    screen: Select,
   },
 
   Result: {
