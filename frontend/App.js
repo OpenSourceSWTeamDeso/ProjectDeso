2bf119a02ec17639113fa644a44d1f7c2f6e6cf4
KiHo Kim <75569308+kiho0042@users.noreply.github.com>
2021-09-09 18:24:16 +0900

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+import { ImageBackground, StyleSheet, Text, View, Image } from "react-native";
+import Navigator from "./routes/homeStack";
+
+const App = () => {
+  return <Navigator />;
+};
+
+export default App;
